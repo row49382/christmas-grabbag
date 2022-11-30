8ff3406ac5ecacab76c377ac976b4ca5dc7560c5
row49382 <r-wroblewski@bethel.edu>
2022-11-30 02:19:38 -0600

    implementing complete logic of selection

--- src/main/java/com/row49382/service/impl/ParticipantWithExemptionsPairingServiceImpl.java
+++ src/main/java/com/row49382/service/impl/ParticipantWithExemptionsPairingServiceImpl.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Random;
 
 public class ParticipantWithExemptionsPairingServiceImpl implements PairingService {
@@ -17,32 +16,42 @@ public class ParticipantWithExemptionsPairingServiceImpl implements PairingServi
 
     public ParticipantWithExemptionsPairingServiceImpl(
             List<Participant> participants,
-            Map<String, String[]> exemptionsByParticipantName) {
+            Map<String, String[]> exemptionsByParticipantName,
+            Random random) {
         this.participants = participants;
         this.exemptionsByParticipantName = exemptionsByParticipantName;
-        this.random = new Random();
+        this.random = random;
     }
 
     public void generatePairings() {
         boolean doesNeedReroll;
+
         do {
+            List<Participant> candidateReceivers = new ArrayList<>(this.participants);
+
             for (int i = 0; i < this.participants.size(); i++) {
                 Participant currentParticipant = this.participants.get(i);
                 List<Integer> candidateIndexes =
-                        this.getCandidateIndexes(currentParticipant.getName(), i);
+                        this.getCandidateIndexes(candidateReceivers, currentParticipant.getName());
 
                 if (!candidateIndexes.isEmpty()) {
                     int candidateIndex = candidateIndexes.get(this.random.nextInt(candidateIndexes.size()));
-                    currentParticipant.setReceiver(this.participants.remove(candidateIndex));
+                    Participant receiver = candidateReceivers.remove(candidateIndex);
+                    receiver.setPicked(true);
+                    currentParticipant.setReceiver(receiver);
+
                 }
             }
 
             if (this.participants.stream()
-                    .map(Participant::getReceiver)
-                    .anyMatch(Objects::isNull)) {
+                    .map(Participant::isPicked)
+                    .anyMatch(p -> p == false)) {
                 // One or more participant's only recipient options were an exemption
                 // we need to reroll again to make sure everyone gets a user without exemption.
-                this.participants.stream().forEach(p -> p.setReceiver(null));
+                this.participants.forEach(p -> {
+                    p.setReceiver(null);
+                    p.setPicked(false);
+                });
                 doesNeedReroll = true;
             }
             else {
@@ -51,19 +60,25 @@ public class ParticipantWithExemptionsPairingServiceImpl implements PairingServi
         } while (doesNeedReroll);
     }
 
-    private List<Integer> getCandidateIndexes(String participantName, int participantIndex) {
+    private List<Integer> getCandidateIndexes(
+            List<Participant> candidateReceivers,
+            String participantName) {
         String[] exemptions = this.exemptionsByParticipantName.get(participantName);
         List<Integer> candidateIndexes = new ArrayList<>();
-        candidateIndexes.add(participantIndex);
 
-        for (int i = 0; i < this.participants.size(); i++) {
-            final Participant currParticipant = this.participants.get(i);
-            if (!currParticipant.isPicked() &&
-                !Arrays.stream(exemptions).anyMatch(e -> e.equals(currParticipant.getName()))) {
+        for (int i = 0; i < candidateReceivers.size(); i++) {
+            final Participant currCandidateReceiver = candidateReceivers.get(i);
+            if (!participantName.equals(currCandidateReceiver.getName()) &&
+                !currCandidateReceiver.isPicked() &&
+                this.isCandidateReceiverNotAnExemption(exemptions, currCandidateReceiver)) {
                 candidateIndexes.add(i);
             }
         }
 
         return candidateIndexes;
+    }
+
+    private boolean isCandidateReceiverNotAnExemption(String[] exemptions, Participant candidateReceiver) {
+        return exemptions == null || !Arrays.stream(exemptions).anyMatch(e -> e.equals(candidateReceiver.getName()));
     }
 }
